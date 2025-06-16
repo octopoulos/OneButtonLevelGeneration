d8a00346ee86ca5bad2a0acb86abbf1795b43b67
ywj <woojae0107@gmail.com>
2025-06-16 14:07:37 +0900

    ?? ???? ? ??? ??? #changelist validated
    [git-p4: depot-paths = "//depot/": change = 14]

---
+++ Source/OneButtonLevelGeneration/Public/Component/OCGComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Data/OCGBiomeSettings.h"
+#include "OCGComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ONEBUTTONLEVELGENERATION_API UOCGComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOCGComponent();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OCG")
+	TArray<FOCGBiomeSettings> Biomes; 
+	
+};
---
+++ Source/OneButtonLevelGeneration/Public/Data/OCGBiomeSettings.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "OCGBiomeSettings.generated.h"
+
+USTRUCT(BlueprintType)
+struct FOCGBiomeStaticMesh
+{
+    GENERATED_BODY()
+	
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    UStaticMesh* StaticMesh;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    int Weight = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    FVector Scale = FVector(1.0f);
+};
+
+USTRUCT(BlueprintType)
+struct FOCGBiomeSettings
+{
+    GENERATED_BODY()
+	
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    FString BiomeName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    float MinHeight = -100.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    float MaxHeight = 1000.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    UMaterialInterface* BiomeMaterial;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    TArray<FOCGBiomeStaticMesh> StaticMeshSettings;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome", meta = (AllowPrivate))
+    int Weight = 1;
+};
