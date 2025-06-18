d6e45f653e3a26b0d6201185b9274107222b155f
gudtldn <gudtldn1234@gmail.com>
2025-06-19 02:26:46 +0900

    wip: landscape pcg
    [git-p4: depot-paths = "//depot/": change = 67]

---
+++ Source/OneButtonLevelGeneration/Private/DataAsset/OCGHierarchyDataAsset.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright (c) 2025 Code1133. All rights reserved.
+
+
+#include "DataAsset/OCGHierarchyDataAsset.h"
---
+++ Source/OneButtonLevelGeneration/Public/DataAsset/OCGHierarchyDataAsset.h
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2025 Code1133. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "OCGHierarchyDataAsset.generated.h"
+
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
+struct FLandscapeHierarchyData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName LayerName;
+};
+
+/**
+ *
+ */
+UCLASS(BlueprintType)
+class ONEBUTTONLEVELGENERATION_API UOCGHierarchyDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FLandscapeHierarchyData> HierarchyData;
+};
