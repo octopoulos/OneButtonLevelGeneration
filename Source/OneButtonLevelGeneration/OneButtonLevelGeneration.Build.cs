8255567d2c8f031b650766f3e85925e7d80752db
gudtldn <gudtldn1234@gmail.com>
2025-06-18 16:12:58 +0900

    feat: Add dependency to Build.cs and .uplugin
    [git-p4: depot-paths = "//depot/": change = 49]

--- Source/OneButtonLevelGeneration/OneButtonLevelGeneration.Build.cs
+++ Source/OneButtonLevelGeneration/OneButtonLevelGeneration.Build.cs
@@ -26,7 +26,9 @@ public class OneButtonLevelGeneration : ModuleRules
 			new string[]
 			{
 				"Core",
-
+				"PCG",
+				"PCGEditor",
+				"PCGGeometryScriptInterop"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
