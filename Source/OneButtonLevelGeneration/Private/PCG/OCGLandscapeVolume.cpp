681d7d15b5474068fac5e32a8cdcf277c7349504
gudtldn <gudtldn1234@gmail.com>
2025-06-18 16:39:12 +0900

    move file
    [git-p4: depot-paths = "//depot/": change = 50]

---
+++ Source/OneButtonLevelGeneration/Private/PCG/OCGLandscapeVolume.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2025 Code1133. All rights reserved.
+
+#include "PCG/OCGLandscapeVolume.h"
+#include "PCGComponent.h"
+#include "Components/BoxComponent.h"
+
+
+AOCGLandscapeVolume::AOCGLandscapeVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BoxComponent->SetCollisionProfileName(TEXT("NoCollision"));
+	BoxComponent->SetGenerateOverlapEvents(false);
+	RootComponent = BoxComponent;
+
+	PCGComponent = CreateDefaultSubobject<UPCGComponent>(TEXT("PCGComponent"));
+}
---
+++ Source/OneButtonLevelGeneration/Public/PCG/OCGLandscapeVolume.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2025 Code1133. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "OCGLandscapeVolume.generated.h"
+
+class UBoxComponent;
+class UPCGComponent;
+
+
+UCLASS()
+class ONEBUTTONLEVELGENERATION_API AOCGLandscapeVolume : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AOCGLandscapeVolume();
+
+private:
+	/** PCG Box Volume */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Box", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> BoxComponent;
+
+	/** PCG Component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PCG", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPCGComponent> PCGComponent;
+};
