8f473eaffa4ccb9cd9f5c27a0f546dc67e6c6126
hans4809 <58892107+hans4809@users.noreply.github.com>
2025-06-18 14:01:40 +0900

    feat : Define Base of OCG Project Class
    [git-p4: depot-paths = "//depot/": change = 44]

---
+++ Source/OneButtonLevelGeneration/Private/OCGLevelGenerator.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OCGLevelGenerator.h"
+
+
+// Sets default values
+AOCGLevelGenerator::AOCGLevelGenerator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOCGLevelGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOCGLevelGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
