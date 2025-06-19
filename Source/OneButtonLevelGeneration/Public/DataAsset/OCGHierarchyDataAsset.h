b17daa9540cbd67096dcccd59c8a008755968e24
gudtldn <gudtldn1234@gmail.com>
2025-06-19 15:14:33 +0900

    feat: Update PCG Landscape Data Asset 한글 테스트 (<- Hangul test)
    [git-p4: depot-paths = "//depot/": change = 73]

--- Source/OneButtonLevelGeneration/Public/DataAsset/OCGHierarchyDataAsset.h
+++ Source/OneButtonLevelGeneration/Public/DataAsset/OCGHierarchyDataAsset.h
@@ -11,12 +11,59 @@
  *
  */
 USTRUCT(BlueprintType)
+struct FMeshAndWeight
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMesh> Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ClampMin = 0, UIMin = 0))
+	int32 Weight = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName LayerName;
+};
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
 struct FLandscapeHierarchyData
 {
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FName LayerName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ClampMin = 0.0f, ClampMax = 1.0f, UIMin = 0.0f, UIMax = 1.0f))
+	float BlendedWeight = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float PointsPerSquareMeter = 0.1f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector PointExtent = FVector{100.0f, 100.0f, 100.0f};
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Looseness = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ClampMin = 0.0f, ClampMax = 1.0f, UIMin = 0.0f, UIMax = 1.0f))
+	float PointSteepness = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 Seed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FLinearColor DebugColor = FLinearColor::White;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FMeshAndWeight> Meshes;
+
+	FLandscapeHierarchyData()
+		: Seed(FMath::Rand())
+	{
+	}
 };
 
 /**
