a76bff3f35300934ff4202b7466ef5150ea37c85
허승빈 <qmfhfl02@gmail.com>
2025-06-27 20:19:12 +0900

    fix: Landscape Settings
    [git-p4: depot-paths = "//depot/": change = 135]

--- Source/OneButtonLevelGeneration/Private/Data/MapPreset.cpp
+++ Source/OneButtonLevelGeneration/Private/Data/MapPreset.cpp
@@ -50,5 +50,36 @@ void UMapPreset::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEv
 	{
 		EditorToolkit->CreateOrUpdateMaterialEditorWrapper(Cast<UMaterialInstanceConstant>(LandscapeMaterial));
 	}
+
+	//Update Landscape Settings
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(ThisClass, Landscape_QuadsPerSection) ||
+		PropertyName == GET_MEMBER_NAME_CHECKED(ThisClass, Landscape_ComponentCount) ||
+		PropertyName == GET_MEMBER_NAME_CHECKED(ThisClass, Landscape_SectionsPerComponent) ||
+		PropertyName == GET_MEMBER_NAME_CHECKED(ThisClass, MapResolution))
+	{
+		// 무한 루프를 방지하기 위해 변경 알림을 잠시 비활성화합니다.
+		FEditorScriptExecutionGuard ScriptGuard;
+
+		// Landscape resolution formula
+		// ComponentSize = QuadsPerSection * SectionsPerComponent
+		// TotalResolution = ComponentSize * ComponentCount + 1
+		int32 ComponentSize;
+		if (PropertyName == GET_MEMBER_NAME_CHECKED(ThisClass, MapResolution))
+		{
+			ComponentSize = static_cast<float>(Landscape_QuadsPerSection) * Landscape_SectionsPerComponent;
+			FIntPoint ComponentCount;
+			ComponentCount.X = (MapResolution.X - 1) / ComponentSize;
+			ComponentCount.Y = (MapResolution.Y - 1) / ComponentSize;
+			Landscape_ComponentCount = ComponentCount;
+		}
+		else
+		{
+			ComponentSize = static_cast<float>(Landscape_QuadsPerSection) * Landscape_SectionsPerComponent;
+			FIntPoint NewMapResolution;
+			NewMapResolution.X = ComponentSize * Landscape_ComponentCount.X + 1;
+			NewMapResolution.Y = ComponentSize * Landscape_ComponentCount.Y + 1;
+			MapResolution = NewMapResolution;
+		}
+	}
 }
 #endif
